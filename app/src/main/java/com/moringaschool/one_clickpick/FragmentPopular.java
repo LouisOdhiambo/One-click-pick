da1471e90c82a8f4677931c1f959eca6b88fb464
Louis-crypto <kinglouis745@gmail.com>
2020-03-02 10:40:59 +0300

    add onCreateView method to fagment popular class

--- app/src/main/java/com/moringaschool/one_clickpick/FragmentPopular.java
+++ app/src/main/java/com/moringaschool/one_clickpick/FragmentPopular.java
@@ -1,4 +1,24 @@
 package com.moringaschool.one_clickpick;
 
-public class FragmentPopular {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentPopular extends Fragment {
+    View view;
+    public FragmentPopular() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.popular_fragment, container, false);
+        return view;
+    }
+
 }
