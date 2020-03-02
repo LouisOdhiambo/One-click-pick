c386d2624d29a619ee443faca2de5b48d2ead6c2
Louis-crypto <kinglouis745@gmail.com>
2020-03-02 10:48:44 +0300

    call view pager adapter in the main activity

--- app/src/main/java/com/moringaschool/one_clickpick/MainActivity.java
+++ app/src/main/java/com/moringaschool/one_clickpick/MainActivity.java
@@ -1,14 +1,45 @@
 package com.moringaschool.one_clickpick;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.orderNowButton) Button mOrderNowButton;
+    @BindView(R.id.mainPageTabLayout) TabLayout tabLayout;
+    @BindView(R.id.viewPagerMainPage) ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mOrderNowButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new FragmentPopular(), "Popular");
+        adapter.addFragment(new FragmentLatest(), "Latest");
+        adapter.addFragment(new FragmentWishList(), "Wishlist");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
