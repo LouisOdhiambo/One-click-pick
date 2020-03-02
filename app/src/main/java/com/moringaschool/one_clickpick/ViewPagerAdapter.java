e6aa669f616958e36c30a270fa7d374568a5a4f2
Louis-crypto <kinglouis745@gmail.com>
2020-03-02 10:59:17 +0300

    add tabslayout to the activity main xml file

--- app/src/main/java/com/moringaschool/one_clickpick/ViewPagerAdapter.java
+++ app/src/main/java/com/moringaschool/one_clickpick/ViewPagerAdapter.java
@@ -1,4 +1,41 @@
 package com.moringaschool.one_clickpick;
 
-public class ViewPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> listOfAllFragemnts = new ArrayList<>();
+    private final List<String> titlesOfAllFragemnts = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position){
+        return listOfAllFragemnts.get(position);
+    }
+
+    @Override
+    public int getCount(){
+        return listOfAllFragemnts.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titlesOfAllFragemnts.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        listOfAllFragemnts.add(fragment);
+        titlesOfAllFragemnts.add(title);
+    }
 }
