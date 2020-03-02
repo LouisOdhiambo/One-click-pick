eaedc202c992ed3fa1308f8be6cc25d59e6551a0
Louis-crypto <kinglouis745@gmail.com>
2020-03-02 10:56:01 +0300

    create view pager adapter which is resposible for switching tabs view

--- app/src/main/java/com/moringaschool/one_clickpick/LoginActivity.java
+++ app/src/main/java/com/moringaschool/one_clickpick/LoginActivity.java
@@ -1,4 +1,37 @@
 package com.moringaschool.one_clickpick;
 
-public class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.logInButton) Button mLogInButton;
+    @BindView(R.id.userEnteredName) EditText mUserEnteredName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = mUserEnteredName.getText().toString();
+                Intent intentCategories = new Intent(LoginActivity.this, CategoriesActivity.class);
+                intentCategories.putExtra("userName", userName);
+                startActivity(intentCategories);
+            }
+        });
+    }
 }
