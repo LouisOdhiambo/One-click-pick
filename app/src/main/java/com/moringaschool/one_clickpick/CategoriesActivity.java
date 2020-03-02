c750ae8977be0d5b210c0af0ec363e8142d49a9a
Louis-crypto <kinglouis745@gmail.com>
2020-03-02 10:45:45 +0300

    add getIntent method to the ctegories activity class

--- app/src/main/java/com/moringaschool/one_clickpick/CategoriesActivity.java
+++ app/src/main/java/com/moringaschool/one_clickpick/CategoriesActivity.java
@@ -1,4 +1,25 @@
 package com.moringaschool.one_clickpick;
 
-public class CategoriesActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CategoriesActivity extends AppCompatActivity {
+    @BindView(R.id.welcomeMessage) TextView mWelcomeMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("userName");
+        mWelcomeMessage.setText("Welcome back " + userName);
+    }
 }
---
+++ app/src/main/java/com/moringaschool/one_clickpick/FragmentLatest.java
@@ -0,0 +1,4 @@
+package com.moringaschool.one_clickpick;
+
+public class FragmentLatest {
+}
