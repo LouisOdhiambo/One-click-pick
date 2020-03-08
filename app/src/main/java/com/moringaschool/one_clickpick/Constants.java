dce9d67d1f7634fd77a58d7d33e0e60f1d1f2cfe
Louis-crypto <kinglouis745@gmail.com>
2020-03-08 12:29:32 +0300

    create constants.java which will contain the api_key and base_url

---
+++ app/src/main/java/com/moringaschool/one_clickpick/Constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.one_clickpick;
+
+public class Constants {
+    public static final String ETSY_BASE_URL = "https://openapi.etsy.com/v2/";
+    public static final String api_key = BuildConfig.ETSY_API_KEY;
+}
